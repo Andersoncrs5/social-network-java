a237364ef3a42fdc7fb1fdbf353842988e5d812b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-05 15:49:27 -0300

    Impled UserRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.blog.writeapi.repositories;
+
+import com.blog.writeapi.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}

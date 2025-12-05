3d09f6af044294dcf0933cf89d57593b8e444d02
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-05 15:22:17 -0300

    init project

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/WriteapiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteapiApplication.class, args);
+	}
+
+}
---
+++ apis/writeapi/src/test/java/com/blog/writeapi/WriteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

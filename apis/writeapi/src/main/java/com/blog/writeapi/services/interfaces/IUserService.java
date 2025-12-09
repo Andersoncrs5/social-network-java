3d405470cf1cf28108533588d5cc2c29ea00e730
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 20:10:07 -0300

    New method Create

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IUserService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IUserService.java
@@ -1,5 +1,6 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.dtos.user.CreateUserDTO;
 import com.blog.writeapi.dtos.user.UpdateUserDTO;
 import com.blog.writeapi.models.UserModel;
 
@@ -10,4 +11,5 @@ public interface IUserService {
     Boolean ExistsById(Long id);
     void Delete(UserModel user);
     UserModel Update(UpdateUserDTO dto, UserModel user);
+    UserModel Create(CreateUserDTO dto);
 }
--- apis/writeapi/src/main/java/com/blog/writeapi/services/providers/UserService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/providers/UserService.java
@@ -1,14 +1,14 @@
 package com.blog.writeapi.services.providers;
 
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.dtos.user.CreateUserDTO;
 import com.blog.writeapi.dtos.user.UpdateUserDTO;
 import com.blog.writeapi.models.UserModel;
 import com.blog.writeapi.repositories.UserRepository;
 import com.blog.writeapi.services.interfaces.IUserService;
 import com.blog.writeapi.utils.mappers.UserMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,6 +18,7 @@ public class UserService implements IUserService {
 
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final Snowflake snowflakeIdGenerator;
 
     @Override
     public Optional<UserModel> GetById(Long id) {
@@ -39,6 +40,15 @@ public class UserService implements IUserService {
         mapper.merge(dto, user);
 
         return this.repository.save(user);
+    }
+
+    @Override
+    public UserModel Create(CreateUserDTO dto) {
+        UserModel user = new UserModel();
+        mapper.merge(dto, user);
+        user.setId(snowflakeIdGenerator.nextId());
+
+        return repository.save(user);
     }
 
 }

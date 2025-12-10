f2ff23a0f0bd37e46ba484d60ba6950edf8eae0b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-10 14:46:32 -0300

    Change name method findByid

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IRoleService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IRoleService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface IRoleService {
     Optional<RoleModel> findByName(String name);
-    Optional<RoleModel> findByid(Long id);
+    Optional<RoleModel> findById(Long id);
     Boolean existsById(Long id);
     Boolean existsByName(String name);
     void delete(RoleModel role);

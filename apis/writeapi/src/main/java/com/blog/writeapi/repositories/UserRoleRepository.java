f6b5b65c3dd59f643fd506fafb974ef29c8ce422
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 21:13:47 -0300

    New methods existsByUserAndRole, findByUserAndRole and findAllByUser

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRoleRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRoleRepository.java
@@ -1,8 +1,16 @@
 package com.blog.writeapi.repositories;
 
+import com.blog.writeapi.models.RoleModel;
+import com.blog.writeapi.models.UserModel;
 import com.blog.writeapi.models.UserRoleModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRoleRepository extends JpaRepository<@NonNull UserRoleModel, @NonNull Long> {
+    Boolean existsByUserAndRole(UserModel user, RoleModel role);
+    Optional<UserRoleModel> findByUserAndRole(UserModel user, RoleModel role);
+    List<UserRoleModel> findAllByUser(UserModel user);
 }

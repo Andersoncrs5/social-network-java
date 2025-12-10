48130b764f294a17d908996a3c4d6392a0b63c5c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-10 10:55:11 -0300

    Impled CustomUserDetailsService

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.blog.writeapi.configs.security;
+
+import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.models.UserRoleModel;
+import com.blog.writeapi.repositories.UserRepository;
+import com.blog.writeapi.repositories.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        List<UserRoleModel> roles = this.userRoleRepository.findAllByUser(user);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                roles.stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getRole().getName()))
+                        .toList()
+        );
+    }
+}

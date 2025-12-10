1b0717cca2099eed6208dc477fbe97666098c4fb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-10 10:55:43 -0300

    New method existsByEmail

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRepository.java
@@ -3,10 +3,14 @@ package com.blog.writeapi.repositories;
 import com.blog.writeapi.models.UserModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull UserModel, @NonNull Long> {
-    UserDetails findByEmail(String email);
+    Optional<UserModel> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }

aaafc5b3a5923e26516d90f6477625d3259ac1a2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-10 14:46:50 -0300

    Added Builder, NoArgsConstructor and AllArgsConstructor

--- apis/writeapi/src/main/java/com/blog/writeapi/models/RoleModel.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/models/RoleModel.java
@@ -1,8 +1,7 @@
 package com.blog.writeapi.models;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,6 +16,9 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 @Setter
 @Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleModel {
     @Id
     @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")

fe04b58801b79372f7b5d6581e379cc113196e85
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-09 21:42:31 -0300

    new method generateToken

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/ITokenService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/ITokenService.java
@@ -1,4 +1,10 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.models.RoleModel;
+import com.blog.writeapi.models.UserModel;
+
+import java.util.List;
+
 public interface ITokenService {
+    String generateToken(UserModel user, List<RoleModel> roles);
 }

582e56748aae88d4a005f356ea6fb47a2b9e3e7a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-11 20:53:29 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/user/CreateUserDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/user/CreateUserDTO.java
@@ -1,8 +1,26 @@
 package com.blog.writeapi.dtos.user;
 
+import com.blog.writeapi.utils.annotations.valid.user.uniqueEmail.UniqueEmail;
+import com.blog.writeapi.utils.annotations.valid.user.uniqueUsername.UniqueUsername;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateUserDTO(
-    String name,
-    String username,
-    String email,
-    String password
+        @Max(100)
+        @NotBlank
+        String name,
+
+        @Max(100)
+        @NotBlank
+        @UniqueUsername
+        String username,
+
+        @Max(150)
+        @NotBlank
+        @UniqueEmail
+        String email,
+
+        @Max(60)
+        @NotBlank
+        String password
 ){}

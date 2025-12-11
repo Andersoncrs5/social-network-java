3c0918dce1088d7ba05adcd82e4fe1a565b01ef9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-11 20:35:08 -0300

    Added annotation Repository

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRoleRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/UserRoleRepository.java
@@ -5,10 +5,12 @@ import com.blog.writeapi.models.UserModel;
 import com.blog.writeapi.models.UserRoleModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<@NonNull UserRoleModel, @NonNull Long> {
     Boolean existsByUserAndRole(UserModel user, RoleModel role);
     Optional<UserRoleModel> findByUserAndRole(UserModel user, RoleModel role);

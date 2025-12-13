91339e2e6941f2df23815eb44d9eca46c0257d54
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-13 20:40:00 -0300

    New endpoint update

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/UserControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/UserControllerDocs.java
@@ -1,5 +1,6 @@
 package com.blog.writeapi.controllers.docs;
 
+import com.blog.writeapi.dtos.user.UpdateUserDTO;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import com.blog.writeapi.utils.res.classes.user.ResponseUserDTO;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,9 +8,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 public interface UserControllerDocs {
 
@@ -37,4 +38,15 @@ public interface UserControllerDocs {
                     schema = @Schema(implementation = ResponseHttp.class)))
     ResponseEntity<?> deleteUser(HttpServletRequest request);
 
+    @PatchMapping
+    @Operation(summary = "Update user", tags = {"User"})
+    @ApiResponse(responseCode = "404",
+            description = "User not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ResponseHttp.class)))
+    @ApiResponse(responseCode = "200",
+            description = "User update",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ResponseUserDTO.class)))
+    ResponseEntity<?> update(@Valid @RequestBody UpdateUserDTO dto, HttpServletRequest request);
 }
--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/UserController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/UserController.java
@@ -1,6 +1,7 @@
 package com.blog.writeapi.controllers.providers;
 
 import com.blog.writeapi.controllers.docs.UserControllerDocs;
+import com.blog.writeapi.dtos.user.UpdateUserDTO;
 import com.blog.writeapi.dtos.user.UserDTO;
 import com.blog.writeapi.models.UserModel;
 import com.blog.writeapi.services.interfaces.ITokenService;
@@ -8,14 +9,17 @@ import com.blog.writeapi.services.interfaces.IUserService;
 import com.blog.writeapi.utils.mappers.UserMapper;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -86,6 +90,61 @@ public class UserController implements UserControllerDocs {
         ResponseHttp<Object> res = new ResponseHttp<>(
                 null,
                 "User deleted",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> update(@Valid @RequestBody UpdateUserDTO dto, HttpServletRequest request) {
+        Long userId = this.tokenService.extractUserIdFromRequest(request);
+
+        Optional<UserModel> opt = this.userService.GetById(userId);
+        if (opt.isEmpty()) {
+            ResponseHttp<Object> res = new ResponseHttp<>(
+                    null,
+                    "User not found",
+                    UUID.randomUUID().toString(),
+                    1,
+                    false,
+                    OffsetDateTime.now()
+            );
+
+            return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+        }
+        UserModel currentUser = opt.get();
+
+        if (dto.username() != null && !dto.username().isBlank()) {
+            String newUsername = dto.username();
+
+            if (!Objects.equals(currentUser.getUsername(), newUsername)) {
+
+                Boolean usernameAlreadyExists = this.userService.existsByUsername(newUsername);
+                if (usernameAlreadyExists) {
+                    ResponseHttp<Object> res = new ResponseHttp<>(
+                            null,
+                            "The Username '" + newUsername + "' is already in use by another user.",
+                            UUID.randomUUID().toString(),
+                            1,
+                            false,
+                            OffsetDateTime.now()
+                    );
+                    return new ResponseEntity<>(res, HttpStatus.CONFLICT);
+                }
+            }
+        }
+
+        UserModel user = this.userService.Update(dto, currentUser);
+
+        UserDTO userDTO = this.mapper.toDTO(user);
+
+        ResponseHttp<UserDTO> res = new ResponseHttp<>(
+                userDTO,
+                "User update",
                 UUID.randomUUID().toString(),
                 1,
                 true,

ba966b3b0dcc8b85d36da2dfda9dddcf94f89690
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-13 19:44:09 -0300

    New param UserDTO userDTO

--- apis/writeapi/src/test/java/com/blog/writeapi/utils/res/ResponseUserTest.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/utils/res/ResponseUserTest.java
@@ -1,9 +1,11 @@
 package com.blog.writeapi.utils.res;
 
 import com.blog.writeapi.dtos.user.CreateUserDTO;
+import com.blog.writeapi.dtos.user.UserDTO;
 
 public record ResponseUserTest(
         ResponseTokens tokens,
-        CreateUserDTO dto
+        CreateUserDTO dto,
+        UserDTO userDTO
 ) {
 }

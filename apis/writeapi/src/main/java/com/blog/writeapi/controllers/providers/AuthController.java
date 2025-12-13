4fe980d6ead3c9d909bcdee275d896f61dca2e98
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-12 21:41:35 -0300

    New endpoint login

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/AuthController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/AuthController.java
@@ -2,8 +2,10 @@ package com.blog.writeapi.controllers.providers;
 
 import com.blog.writeapi.controllers.docs.AuthControllerDocs;
 import com.blog.writeapi.dtos.user.CreateUserDTO;
+import com.blog.writeapi.dtos.user.LoginUserDTO;
 import com.blog.writeapi.models.RoleModel;
 import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.models.UserRoleModel;
 import com.blog.writeapi.services.interfaces.IRoleService;
 import com.blog.writeapi.services.interfaces.ITokenService;
 import com.blog.writeapi.services.interfaces.IUserRoleService;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
@@ -32,20 +35,61 @@ public class AuthController implements AuthControllerDocs {
     private final IRoleService roleService;
     private final IUserRoleService userRoleService;
     private final ITokenService tokenService;
+    private final Argon2PasswordEncoder encoder;
+
+    @Override
+    public ResponseEntity<?> login(@Valid @RequestBody LoginUserDTO dto, HttpServletRequest request) {
+
+        Optional<UserModel> optional = this.userService.findByEmail(dto.email());
+        if (optional.isEmpty()) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        UserModel user = optional.get();
+
+        boolean matches = this.encoder.matches(dto.password(), user.getPassword());
+        if (!matches) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        List<RoleModel> roles = user.getRoles().stream().map(UserRoleModel::getRole).toList();
+
+        String token = this.tokenService.generateToken(user, roles);
+        String refreshToken = this.tokenService.generateRefreshToken(user);
+
+        user.setRefreshToken(refreshToken);
+
+        this.userService.UpdateSimple(user);
+
+        ResponseTokens tokens = new ResponseTokens(
+                token,
+                refreshToken
+        );
+
+        ResponseHttp<ResponseTokens> response = new ResponseHttp<>(
+                tokens,
+                "Welcome again",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
     @Override
     public ResponseEntity<?> Create(@Valid @RequestBody CreateUserDTO dto, HttpServletRequest request) {
-
-        UserModel created = this.userService.Create(dto);
+        UserModel user = this.userService.Create(dto);
 
         Optional<RoleModel> roleOpt = this.roleService.findByName("USER_ROLE");
 
         if (roleOpt.isEmpty()) throw new RuntimeException("Role USER_ROLE not exists");
 
-        this.userRoleService.create(created, roleOpt.get());
+        this.userRoleService.create(user, roleOpt.get());
 
-        String token = this.tokenService.generateToken(created, List.of(roleOpt.get()));
-        String refreshToken = this.tokenService.generateRefreshToken(created);
+        String token = this.tokenService.generateToken(user, List.of(roleOpt.get()));
+        String refreshToken = this.tokenService.generateRefreshToken(user);
+
+        user.setRefreshToken(refreshToken);
+
+        this.userService.UpdateSimple(user);
 
         ResponseTokens tokens = new ResponseTokens(
                 token,

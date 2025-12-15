50cac80ca6de3a200577dc00582d6d1f1cd9df02
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-15 20:59:40 -0300

    New ExistsByCategoryId

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/annotations/valid/category/existsByCategoryId/ExistsByCategoryId.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.utils.annotations.valid.category.existsByCategoryId;
+
+import com.blog.writeapi.utils.annotations.valid.category.uniqueName.UniqueCategoryNameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ExistsByCategoryIdValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistsByCategoryId {
+
+    String message() default "Category does not exist.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/annotations/valid/category/existsByCategoryId/ExistsByCategoryIdValidator.java
@@ -0,0 +1,21 @@
+package com.blog.writeapi.utils.annotations.valid.category.existsByCategoryId;
+
+import com.blog.writeapi.repositories.CategoryRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ExistsByCategoryIdValidator implements ConstraintValidator<ExistsByCategoryId, Long> {
+
+    private final CategoryRepository repository;
+
+    @Override
+    public boolean isValid(Long s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null || s <= 0) return true;
+
+        return !this.repository.existsById(s);
+    }
+}

95b3dc1376a11a175445174ea06c90c888458e44
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-18 21:43:21 -0300

    New methods

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/TagRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/TagRepository.java
@@ -5,6 +5,13 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<@NonNull TagModel, @NonNull Long> {
+    Optional<TagModel> findByName(String name);
+    Boolean existsByName(String name);
+
+    Optional<TagModel> findBySlug(String slug);
+    Boolean existsBySlug(String slug);
 }

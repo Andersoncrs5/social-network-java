66a01f39c89b6e5c86aa02ea90d4ca9ad974f193
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-18 22:09:15 -0300

    Adding validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
@@ -1,14 +1,33 @@
 package com.blog.writeapi.dtos.tag;
 
-import java.time.OffsetDateTime;
+import com.blog.writeapi.utils.annotations.valid.global.StringClear.StringClear;
+import com.blog.writeapi.utils.annotations.valid.global.slug.Slug;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateTagDTO(
+
+        @NotBlank
+        @StringClear
+        @Size(min = 2, max = 70)
         String name,
+
+        @Slug
+        @NotBlank
+        @Size(min = 2, max = 80)
         String slug,
+
+        @Size(max = 200)
         String description,
+
+        @NotNull
         Boolean isActive,
+
+        @NotNull
         Boolean isVisible,
-        Boolean isSystem,
-        OffsetDateTime lastUsedAt
+
+        @NotNull
+        Boolean isSystem
 ) {
 }

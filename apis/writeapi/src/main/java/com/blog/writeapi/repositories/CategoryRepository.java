097b9bd745580b0155c1ac776300970561ab8e41
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 16:08:39 -0300

    New method findByIdForUpdate

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/CategoryRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/CategoryRepository.java
@@ -1,8 +1,14 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.CategoryModel;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +17,9 @@ public interface CategoryRepository extends JpaRepository<@NonNull CategoryModel
     Optional<CategoryModel> findByName(String name);
     Optional<CategoryModel> findBySlug(String slug);
     Boolean existsBySlug(String slug);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT t FROM CategoryModel t WHERE t.id = :id")
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "4000")})
+    Optional<CategoryModel> findByIdForUpdate(@Param("id") Long id);
 }

692faaeea776e62f1cecb79614ab0d6eb1e91f8a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-18 22:10:25 -0300

    Adding validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/UpdateTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/UpdateTagDTO.java
@@ -1,11 +1,29 @@
 package com.blog.writeapi.dtos.tag;
 
+import com.blog.writeapi.utils.annotations.valid.global.StringClear.StringClear;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagById.ExistsTagById;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.OffsetDateTime;
 
 public record UpdateTagDTO(
+        @NotNull
+        @ExistsTagById
+        Long id,
+
+        @StringClear
+        @Size(max = 70)
         String name,
+
+        @NotBlank
+        @Size(max = 80)
         String slug,
+
+        @Size(max = 200)
         String description,
+
         Boolean isActive,
         Boolean isVisible,
         Boolean isSystem,

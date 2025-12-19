93ddb9384614349bde7b3ffb98355b4365408cb6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 16:07:58 -0300

    More validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/UpdateTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/UpdateTagDTO.java
@@ -1,24 +1,29 @@
 package com.blog.writeapi.dtos.tag;
 
 import com.blog.writeapi.utils.annotations.valid.global.StringClear.StringClear;
+import com.blog.writeapi.utils.annotations.valid.global.slug.Slug;
 import com.blog.writeapi.utils.annotations.valid.tag.existsTagById.ExistsTagById;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagByName.ExistsTagByName;
+import com.blog.writeapi.utils.annotations.valid.tag.existsTagBySlug.ExistsTagBySlug;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.time.OffsetDateTime;
-
 public record UpdateTagDTO(
+
         @NotNull
         @ExistsTagById
         Long id,
 
         @StringClear
         @Size(max = 70)
+        @ExistsTagByName
         String name,
 
         @NotBlank
         @Size(max = 80)
+        @Slug
+        @ExistsTagBySlug
         String slug,
 
         @Size(max = 200)
@@ -27,6 +32,6 @@ public record UpdateTagDTO(
         Boolean isActive,
         Boolean isVisible,
         Boolean isSystem,
-        OffsetDateTime lastUsedAt
+        java.time.OffsetDateTime lastUsedAt
 ) {
 }

5b73f529131c705ff356b24d98b3890aeb6fde36
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-20 15:17:35 -0300

    Added BaseEntity to tables

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/bases/models/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.blog.writeapi.utils.bases.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private OffsetDateTime updatedAt;
+}

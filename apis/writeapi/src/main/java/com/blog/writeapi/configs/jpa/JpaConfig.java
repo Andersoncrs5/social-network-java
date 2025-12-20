63db99904aad3ea6fd97cab30a742fe7f951afa2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 21:22:47 -0300

    New Configuration to JpaConfig

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/jpa/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.configs.jpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class JpaConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
+}

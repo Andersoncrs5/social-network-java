3a63b48e39a94e5e5ead4ae1df51691be1d7c313
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-20 14:39:31 -0300

    Added annotations Transactional

--- apis/writeapi/src/main/java/com/blog/writeapi/services/providers/RoleService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/providers/RoleService.java
@@ -7,6 +7,7 @@ import com.blog.writeapi.services.interfaces.IRoleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -19,31 +20,37 @@ public class RoleService implements IRoleService {
     private final Snowflake snowflakeIdGenerator;
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<RoleModel> findByName(String name) {
         return this.repository.findByName(name);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<RoleModel> findById(Long id) {
         return this.repository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Boolean existsById(Long id) {
         return this.repository.existsById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Boolean existsByName(String name) {
         return this.repository.existsByName(name);
     }
 
     @Override
+    @Transactional
     public void delete(RoleModel role) {
         this.repository.delete(role);
     }
 
     @Override
+    @Transactional
     public RoleModel create(RoleModel role) {
         role.setId(snowflakeIdGenerator.nextId());
         role.setName(role.getName().toUpperCase());

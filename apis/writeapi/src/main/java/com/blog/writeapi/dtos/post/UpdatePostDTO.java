dd359bc3a5de43e26e16a8c020c1310b418827fc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-20 19:53:26 -0300

    Impled UpdatePostDTO

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/post/UpdatePostDTO.java
@@ -0,0 +1,15 @@
+package com.blog.writeapi.dtos.post;
+
+import com.blog.writeapi.models.enums.Post.PostStatusEnum;
+
+public record UpdatePostDTO(
+        Long id,
+        String title,
+        String slug,
+        String content,
+        PostStatusEnum status,
+        Integer readingTime,
+        Double rankingScore,
+        Boolean isFeatured
+) {
+}

08570e700e5796c35058b4b7c38f7c04e43b7ed7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-19 21:22:27 -0300

    Added annotations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/tag/CreateTagDTO.java
@@ -2,8 +2,8 @@ package com.blog.writeapi.dtos.tag;
 
 import com.blog.writeapi.utils.annotations.valid.global.StringClear.StringClear;
 import com.blog.writeapi.utils.annotations.valid.global.slug.Slug;
-import com.blog.writeapi.utils.annotations.valid.tag.existsTagByName.ExistsTagByName;
-import com.blog.writeapi.utils.annotations.valid.tag.existsTagBySlug.ExistsTagBySlug;
+import com.blog.writeapi.utils.annotations.valid.tag.uniqueTagByName.UniqueTagByName;
+import com.blog.writeapi.utils.annotations.valid.tag.uniqueTagBySlug.UniqueTagBySlug;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,12 +13,12 @@ public record CreateTagDTO(
         @NotBlank
         @StringClear
         @Size(min = 2, max = 70)
-        @ExistsTagByName
+        @UniqueTagByName
         String name,
 
         @Slug
         @NotBlank
-        @ExistsTagBySlug
+        @UniqueTagBySlug
         @Size(min = 2, max = 80)
         String slug,
 

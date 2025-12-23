d343ea84243b91859158802714ed09b2ff338e3d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-23 16:42:19 -0300

    New annotation IsPostAuthor

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/annotations/valid/post/isPostAuthor/IsPostAuthor.java
@@ -0,0 +1,14 @@
+package com.blog.writeapi.utils.annotations.valid.post.isPostAuthor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@postSecurity.isAuthor(#id, T(com.blog.writeapi.utils.security.SecurityUtils).getUserEmail())")
+public @interface IsPostAuthor {
+}

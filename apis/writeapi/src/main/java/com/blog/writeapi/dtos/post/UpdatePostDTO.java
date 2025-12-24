aa5791f9d02d7529f2f0a2b5740af6081d50d3e9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-24 13:43:11 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/post/UpdatePostDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/post/UpdatePostDTO.java
@@ -1,15 +1,24 @@
 package com.blog.writeapi.dtos.post;
 
-import com.blog.writeapi.models.enums.Post.PostStatusEnum;
+import com.blog.writeapi.utils.annotations.valid.global.slug.Slug;
+import com.blog.writeapi.utils.annotations.valid.post.uniqueSlugByPost.UniqueSlugByPost;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
 
 public record UpdatePostDTO(
-        Long id,
+
+        @Size(min = 10, max = 200)
         String title,
+
+        @Slug
+        @UniqueSlugByPost
+        @Size(min = 10, max = 255)
         String slug,
+
+        @Size(min = 50, max = 3000)
         String content,
-        PostStatusEnum status,
-        Integer readingTime,
-        Double rankingScore,
-        Boolean isFeatured
+
+        @Max(240)
+        Integer readingTime
 ) {
 }

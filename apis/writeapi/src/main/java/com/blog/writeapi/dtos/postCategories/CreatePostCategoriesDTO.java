bc71deb74c1cd1024eb0547e9e2d10bd2a062889
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-26 14:29:27 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/postCategories/CreatePostCategoriesDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/postCategories/CreatePostCategoriesDTO.java
@@ -1,8 +1,21 @@
 package com.blog.writeapi.dtos.postCategories;
 
+import com.blog.writeapi.utils.annotations.valid.PostCategories.uniquePrimairyInPost.UniquePrimaryCategoryInPost;
+import com.blog.writeapi.utils.annotations.valid.category.existsByCategoryId.ExistsByCategoryId;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.annotations.valid.post.existsPostById.ExistsPostById;
+
+@UniquePrimaryCategoryInPost
 public record CreatePostCategoriesDTO(
+
+        @IsId
+        @ExistsPostById
         Long postId,
+
+        @IsId
+        @ExistsByCategoryId
         Long categoryId,
+
         Integer displayOrder,
         boolean primary,
         boolean active

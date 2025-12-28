9ad7f32a7b5a1f8f975d3537d46ae99b73a31995
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-28 15:32:35 -0300

    More tests

--- apis/writeapi/src/test/java/com/blog/writeapi/integration/PostTagControllerTest.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/integration/PostTagControllerTest.java
@@ -1,10 +1,10 @@
 package com.blog.writeapi.integration;
 
 import com.blog.writeapi.HelperTest;
-import com.blog.writeapi.dtos.category.CategoryDTO;
 import com.blog.writeapi.dtos.post.PostDTO;
 import com.blog.writeapi.dtos.postTag.CreatePostTagDTO;
 import com.blog.writeapi.dtos.postTag.PostTagDTO;
+import com.blog.writeapi.dtos.postTag.UpdatePostTagDTO;
 import com.blog.writeapi.dtos.tag.TagDTO;
 import com.blog.writeapi.repositories.PostRepository;
 import com.blog.writeapi.repositories.TagRepository;
@@ -119,6 +119,191 @@ public class PostTagControllerTest {
         assertThat(response.data()).isNull();
     }
 
+    // DELETE
+    @Test
+    void shouldRemoveTagOfPost() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+        ResponseUserTest userTest = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        MvcResult result = this.mockMvc.perform(delete(this.URL + "/" + postTagDTO.id())
+                        .header("Authorization", "Bearer " + userTest.tokens().token()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<Object>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<Object> response =
+                objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data()).isNull();
+    }
+
+    @Test
+    void shouldReturnForbTheRemoveTagOfPostBecauseUserNotOwer() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+        ResponseUserTest userTest = this.helper.createUser();
+        ResponseUserTest userTest2 = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        this.mockMvc.perform(delete(this.URL + "/" + postTagDTO.id())
+                        .header("Authorization", "Bearer " + userTest2.tokens().token()))
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    // UPDATE
+    @Test
+    void shouldUpdateAllFieldsPostTag() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+        ResponseUserTest userTest = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        UpdatePostTagDTO dto = new UpdatePostTagDTO(
+                !postTagDTO.active(),
+                !postTagDTO.visible()
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + postTagDTO.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userTest.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<PostTagDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<PostTagDTO> response =
+                objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(postTagDTO.id());
+        assertThat(response.data().active()).isEqualTo(dto.active());
+        assertThat(response.data().visible()).isEqualTo(dto.visible());
+        assertThat(response.data().post().id()).isEqualTo(postDTO.id());
+        assertThat(response.data().tag().id()).isEqualTo(tagDTO.id());
+
+    }
+
+    @Test
+    void shouldUpdateJustFieldActivePostTag() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+        ResponseUserTest userTest = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        UpdatePostTagDTO dto = new UpdatePostTagDTO(
+                !postTagDTO.active(),
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + postTagDTO.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userTest.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<PostTagDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<PostTagDTO> response =
+                objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(postTagDTO.id());
+        assertThat(response.data().active()).isEqualTo(dto.active());
+        assertThat(response.data().visible()).isEqualTo(postTagDTO.visible());
+        assertThat(response.data().post().id()).isEqualTo(postDTO.id());
+        assertThat(response.data().tag().id()).isEqualTo(tagDTO.id());
+
+    }
+
+    @Test
+    void shouldUpdateJustFieldVisiblePostTag() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+        ResponseUserTest userTest = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        UpdatePostTagDTO dto = new UpdatePostTagDTO(
+                null,
+                !postTagDTO.visible()
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + postTagDTO.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userTest.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<PostTagDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<PostTagDTO> response =
+                objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(postTagDTO.id());
+        assertThat(response.data().active()).isEqualTo(postTagDTO.active());
+        assertThat(response.data().visible()).isEqualTo(dto.visible());
+        assertThat(response.data().post().id()).isEqualTo(postDTO.id());
+        assertThat(response.data().tag().id()).isEqualTo(tagDTO.id());
+
+    }
+
+    @Test
+    void shouldReturnForbWhenUpdatePostTag() throws Exception {
+        ResponseUserTest superAdm = this.helper.loginSuperAdm();
+
+        ResponseUserTest userTest = this.helper.createUser();
+        ResponseUserTest userTest2 = this.helper.createUser();
+
+        TagDTO tagDTO = this.helper.createTag(superAdm);
+        PostDTO postDTO = this.helper.createPost(userTest);
+        PostTagDTO postTagDTO = this.helper.addTagToPost(userTest, postDTO, tagDTO);
+
+        UpdatePostTagDTO dto = new UpdatePostTagDTO(
+                null,
+                !postTagDTO.visible()
+        );
+
+        this.mockMvc.perform(patch(this.URL + "/" + postTagDTO.id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
+                        .header("Authorization", "Bearer " + userTest2.tokens().token()
+                        ))
+                .andExpect(status().isForbidden());
+
+    }
 
 
 }

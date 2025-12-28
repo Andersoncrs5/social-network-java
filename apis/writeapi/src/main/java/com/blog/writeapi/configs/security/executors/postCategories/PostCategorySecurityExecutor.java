468d929e8b2b74363fdbdbeb69c6af5e2ecd3cb5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-12-28 16:10:00 -0300

    Improvements in logic in method isAuthor

--- apis/writeapi/src/main/java/com/blog/writeapi/configs/security/executors/postCategories/PostCategorySecurityExecutor.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/security/executors/postCategories/PostCategorySecurityExecutor.java
@@ -1,6 +1,8 @@
 package com.blog.writeapi.configs.security.executors.postCategories;
 
 import com.blog.writeapi.services.interfaces.IPostCategoriesService;
+import com.blog.writeapi.utils.annotations.valid.global.emailConstraint.EmailConstraint;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +12,14 @@ public class PostCategorySecurityExecutor {
     @Autowired
     private IPostCategoriesService service;
 
-    public boolean isAuthor(Long id, String email) {
+    public boolean isAuthor(@IsId Long id, @EmailConstraint String email) {
         if (id == null || email == null) return false;
 
-        return service.getById(id)
-                .map(pc -> pc.getPost().getAuthor().getEmail().equals(email))
-                .orElse(false);
+        return service.getByIdSimple(id)
+                .getPost()
+                .getAuthor()
+                .getEmail()
+                .equalsIgnoreCase(email);
     }
 
 }

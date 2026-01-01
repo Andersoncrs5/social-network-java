ddae32038dd3e35c77e5a6ea0b1e0513346c7fd2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-01 16:49:00 -0300

    Added maps to LocalDateTime

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/PostFavoriteMapper.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/PostFavoriteMapper.java
@@ -5,10 +5,22 @@ import com.blog.writeapi.models.PostFavoriteModel;
 import jakarta.validation.constraints.NotNull;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 @Mapper(componentModel = "spring")
 public interface PostFavoriteMapper {
 
     PostFavoriteDTO toDTO(@NotNull PostFavoriteModel model);
     PostFavoriteModel toModel(@NotNull PostFavoriteDTO dto);
+
+    default OffsetDateTime map(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.atOffset(ZoneOffset.UTC);
+    }
+
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : offsetDateTime.toLocalDateTime();
+    }
 
 }

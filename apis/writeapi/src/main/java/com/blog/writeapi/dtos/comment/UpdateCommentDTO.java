5a2dbb82b7abd6f59d8fe29ad2c3c998d0afa979
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-01 13:14:43 -0300

    Removed field id and any annotations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/comment/UpdateCommentDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/comment/UpdateCommentDTO.java
@@ -1,21 +1,9 @@
 package com.blog.writeapi.dtos.comment;
 
-import com.blog.writeapi.models.enums.comment.CommentStatusEnum;
-import com.blog.writeapi.utils.annotations.valid.comment.existsCommentById.ExistsCommentById;
-import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateCommentDTO(
-        @IsId
-        @ExistsCommentById
-        Long id,
-
-
-        @NotBlank
-        @Size(min = 1, max = 600)
-        String content,
-
-        CommentStatusEnum status
+        @Size(max = 600)
+        String content
 ) {
 }

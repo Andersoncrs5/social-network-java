76e3b2802396203ad75d74bc36eff683e27c4db6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-06 16:13:40 -0300

    Impled methods

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/ICommentFavoriteService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/ICommentFavoriteService.java
@@ -1,4 +1,16 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.models.CommentFavoriteModel;
+import com.blog.writeapi.models.CommentModel;
+import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+
+import java.util.Optional;
+
 public interface ICommentFavoriteService {
+    CommentFavoriteModel add(UserModel user, CommentModel comment);
+    CommentFavoriteModel getById(@IsId Long id);
+    CommentFavoriteModel getByCommentIdAndUserId(CommentModel comment, UserModel user);
+    Optional<CommentFavoriteModel> findByCommentIdAndUserId(CommentModel comment, UserModel user);
+    void remove(CommentFavoriteModel model);
 }
--- apis/writeapi/src/main/java/com/blog/writeapi/services/providers/CommentFavoriteService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/providers/CommentFavoriteService.java
@@ -1,10 +1,20 @@
 package com.blog.writeapi.services.providers;
 
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.models.CommentFavoriteModel;
+import com.blog.writeapi.models.CommentModel;
+import com.blog.writeapi.models.UserModel;
 import com.blog.writeapi.repositories.CommentFavoriteRepository;
 import com.blog.writeapi.services.interfaces.ICommentFavoriteService;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.exceptions.ModelNotFoundException;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -12,5 +22,49 @@ import org.springframework.stereotype.Service;
 public class CommentFavoriteService implements ICommentFavoriteService {
 
     private final CommentFavoriteRepository repository;
+    private final Snowflake generator;
+
+    @Override
+    @Transactional(readOnly = true)
+    public CommentFavoriteModel getById(@IsId Long id) {
+        return this.repository.findById(id).orElseThrow(
+                () -> new ModelNotFoundException("Comment marked with favorite was not found")
+        );
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public CommentFavoriteModel getByCommentIdAndUserId(CommentModel comment, UserModel user) {
+        return this.repository.findByCommentAndUser(comment, user).orElseThrow(
+                () -> new ModelNotFoundException("Comment marked with favorite was not found")
+        );
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<CommentFavoriteModel> findByCommentIdAndUserId(CommentModel comment, UserModel user) {
+        return this.repository.findByCommentAndUser(comment, user);
+    }
+
+
+    @Override
+    @Transactional
+    @Retry(name = "create-retry")
+    public CommentFavoriteModel add(UserModel user, CommentModel comment) {
+        CommentFavoriteModel favor = new CommentFavoriteModel().toBuilder()
+                .id(generator.nextId())
+                .user(user)
+                .comment(comment)
+                .build();
+
+        return this.repository.save(favor);
+    }
+
+    @Override
+    @Transactional
+    @Retry(name = "delete-retry")
+    public void remove(CommentFavoriteModel model) {
+        this.repository.delete(model);
+    }
 
 }

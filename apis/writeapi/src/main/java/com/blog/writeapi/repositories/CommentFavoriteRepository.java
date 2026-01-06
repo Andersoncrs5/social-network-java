38819418696cd7f0ebfa010d0ef6096a98d2c2a5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-06 16:13:44 -0300

    new method

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/CommentFavoriteRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/CommentFavoriteRepository.java
@@ -1,8 +1,13 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.CommentFavoriteModel;
+import com.blog.writeapi.models.CommentModel;
+import com.blog.writeapi.models.UserModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentFavoriteRepository extends JpaRepository<@NonNull CommentFavoriteModel, @NonNull Long> {
+    Optional<CommentFavoriteModel> findByCommentAndUser(CommentModel comment, UserModel user);
 }

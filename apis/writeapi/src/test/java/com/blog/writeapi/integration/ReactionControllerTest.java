8745516ca2624d4a5d1ce77b5ce873a34d3fe6ee
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-11 18:29:22 -0300

    More tests

--- apis/writeapi/src/test/java/com/blog/writeapi/integration/ReactionControllerTest.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/integration/ReactionControllerTest.java
@@ -3,6 +3,7 @@ package com.blog.writeapi.integration;
 import com.blog.writeapi.HelperTest;
 import com.blog.writeapi.dtos.reaction.CreateReactionDTO;
 import com.blog.writeapi.dtos.reaction.ReactionDTO;
+import com.blog.writeapi.dtos.reaction.UpdateReactionDTO;
 import com.blog.writeapi.models.enums.reaction.ReactionTypeEnum;
 import com.blog.writeapi.repositories.ReactionRepository;
 import com.blog.writeapi.utils.res.ResponseHttp;
@@ -19,8 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -103,5 +103,334 @@ public class ReactionControllerTest {
 
         assertThat(response.data()).isNull();
     }
+
+    @Test
+    void shouldUpdateAllFieldsReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                reaction.name() + "a",
+                "https://preview.redd.it/pochita-art-v0-ckhgpbbt06lc1.jpg?width=640&crop=smart&auto=webp&s=c28e059a9408e6c1aad6116b37233ea58ac1a239",
+                "U+1F525",
+                reaction.displayOrder() + 1,
+                !reaction.active(),
+                !reaction.visible(),
+                ReactionTypeEnum.AWARD
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(dto.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(dto.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(dto.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(dto.displayOrder());
+        assertThat(response.data().active()).isEqualTo(dto.active());
+        assertThat(response.data().visible()).isEqualTo(dto.visible());
+        assertThat(response.data().type()).isEqualTo(dto.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldNameReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                reaction.name() + "a",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(dto.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldEmojiUrlReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                "https://preview.redd.it/pochita-art-v0-ckhgpbbt06lc1.jpg?width=640&crop=smart&auto=webp&s=c28e059a9408e6c1aad6116b37233ea58ac1a239",
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(dto.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldEmojiUnicodeReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                null,
+                "0x1F600",
+                null,
+                null,
+                null,
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(dto.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldDisplayOrderReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                null,
+                null,
+                11233L,
+                null,
+                null,
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(dto.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldActiveReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                null,
+                null,
+                null,
+                !reaction.active(),
+                null,
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(dto.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldVisibleReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                null,
+                null,
+                null,
+                null,
+                !reaction.visible(),
+                null
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(dto.visible());
+        assertThat(response.data().type()).isEqualTo(reaction.type());
+    }
+
+    @Test
+    void shouldUpdateJustFieldTypeReaction() throws Exception {
+        ResponseUserTest userData = helper.loginSuperAdm();
+        ReactionDTO reaction = this.helper.createReaction(userData);
+
+        UpdateReactionDTO dto = new UpdateReactionDTO(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                ReactionTypeEnum.AWARD
+        );
+
+        MvcResult result = this.mockMvc.perform(patch(this.URL + "/" + reaction.id())
+                        .content(this.objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + userData.tokens().token()
+                        ))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        TypeReference<ResponseHttp<ReactionDTO>> typeRef = new TypeReference<>() {};
+
+        ResponseHttp<ReactionDTO> response = objectMapper.readValue(json, typeRef);
+
+        assertThat(response.message()).isNotBlank();
+        assertThat(response.status()).isEqualTo(true);
+
+        assertThat(response.data().id()).isEqualTo(reaction.id());
+        assertThat(response.data().name()).isEqualTo(reaction.name());
+        assertThat(response.data().emojiUrl()).isEqualTo(reaction.emojiUrl());
+        assertThat(response.data().emojiUnicode()).isEqualTo(reaction.emojiUnicode());
+        assertThat(response.data().displayOrder()).isEqualTo(reaction.displayOrder());
+        assertThat(response.data().active()).isEqualTo(reaction.active());
+        assertThat(response.data().visible()).isEqualTo(reaction.visible());
+        assertThat(response.data().type()).isEqualTo(dto.type());
+    }
+
 
 }

1f82e4c4f39a1113cfef71825a13afdf5eb03ca6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-11 17:22:46 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/reaction/CreateReactionDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/reaction/CreateReactionDTO.java
@@ -1,14 +1,32 @@
 package com.blog.writeapi.dtos.reaction;
 
 import com.blog.writeapi.models.enums.reaction.ReactionTypeEnum;
+import com.blog.writeapi.utils.annotations.valid.reaction.uniqueReactionEmojiUnicode.UniqueReactionEmojiUnicode;
+import com.blog.writeapi.utils.annotations.valid.reaction.uniqueReactionName.UniqueReactionName;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateReactionDTO(
+
+        @UniqueReactionName
         String name,
+
+        @Size(max = 1200)
         String emojiUrl,
+
+        @UniqueReactionEmojiUnicode
         String emojiUnicode,
+
+        @NotNull
         Long displayOrder,
+
+        @NotNull
         Boolean active,
+
+        @NotNull
         Boolean visible,
+
+        @NotNull
         ReactionTypeEnum type
 ) {
 }

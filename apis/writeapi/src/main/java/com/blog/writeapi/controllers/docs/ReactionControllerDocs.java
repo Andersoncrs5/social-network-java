1afdf50f8aba7c57711a160490c12751e247640b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-11 18:29:06 -0300

    Impled endpoints

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/ReactionControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/ReactionControllerDocs.java
@@ -1,4 +1,127 @@
 package com.blog.writeapi.controllers.docs;
 
+import com.blog.writeapi.dtos.reaction.CreateReactionDTO;
+import com.blog.writeapi.dtos.reaction.UpdateReactionDTO;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.res.ResponseHttp;
+import com.blog.writeapi.utils.res.swagger.reaction.ResponseReactionDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Reaction", description = "Endpoints for managing reaction")
 public interface ReactionControllerDocs {
+
+    @PostMapping
+    @CircuitBreaker(name = "tagCreateCB")
+    @Operation(
+            summary = "Create a new reaction",
+            description = "Registers a new reaction type in the system catalog. This is typically an administrative operation."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "reaction created successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseReactionDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data (Validation Error)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "Service temporarily unavailable (Circuit Breaker active)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            )
+    })
+    ResponseEntity<?> create(
+            @Valid @RequestBody CreateReactionDTO dto,
+            HttpServletRequest request
+    );
+
+    @DeleteMapping("/{id}")
+    @CircuitBreaker(name = "tagDeleteCB")
+    @Operation(
+            summary = "Delete a reaction type",
+            description = "Removes a reaction type from the system catalog by its ID.",
+            method = "DELETE"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Reaction deleted successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid ID provided (Validation Error)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Reaction not found",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "Service temporarily unavailable (Circuit Breaker active)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            )
+    })
+    ResponseEntity<?> delete(
+            @Parameter(description = "The Snowflake ID of the comment", example = "1998780200074176609")
+            @IsId @PathVariable Long id,
+            HttpServletRequest request
+    );
+
+    @PatchMapping("/{id}")
+    @CircuitBreaker(name = "tagUpdateCB")
+    @Operation(
+            summary = "Update an existing reaction",
+            description = "Updates specific fields of an existing reaction type. Only provided fields will be modified."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Reaction updated successfully",
+                    content = @Content(schema = @Schema(implementation = ResponseReactionDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data or ID (Validation Error)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Reaction not found",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Error internal in server",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            ),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "Service temporarily unavailable (Circuit Breaker active)",
+                    content = @Content(schema = @Schema(implementation = ResponseHttp.class))
+            )
+    })
+    ResponseEntity<?> update(
+            @Parameter(description = "The Snowflake ID of the reaction", example = "1998780200074176609")
+            @IsId @PathVariable Long id,
+            @Valid @RequestBody UpdateReactionDTO dto,
+            HttpServletRequest request
+    );
+
 }
--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/ReactionController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/providers/ReactionController.java
@@ -1,14 +1,28 @@
 package com.blog.writeapi.controllers.providers;
 
 import com.blog.writeapi.controllers.docs.ReactionControllerDocs;
+import com.blog.writeapi.dtos.reaction.CreateReactionDTO;
+import com.blog.writeapi.dtos.reaction.ReactionDTO;
+import com.blog.writeapi.dtos.reaction.UpdateReactionDTO;
+import com.blog.writeapi.models.ReactionModel;
 import com.blog.writeapi.services.interfaces.IReactionService;
-import com.blog.writeapi.services.providers.ReactionService;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
 import com.blog.writeapi.utils.mappers.ReactionMapper;
+import com.blog.writeapi.utils.res.ResponseHttp;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Slf4j
 @Validated
@@ -20,6 +34,64 @@ public class ReactionController implements ReactionControllerDocs {
     private final IReactionService service;
     private final ReactionMapper mapper;
 
+    @Override
+    public ResponseEntity<?> create(
+            @Valid @RequestBody CreateReactionDTO dto,
+            HttpServletRequest request
+    ) {
+        ReactionModel reaction = this.service.create(dto);
 
+        ResponseHttp<ReactionDTO> res = new ResponseHttp<>(
+                this.mapper.toDTO(reaction),
+                "Reaction created successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @Override
+    public ResponseEntity<?> delete(
+            @IsId @PathVariable Long id,
+            HttpServletRequest request
+    ) {
+        ReactionModel reaction = this.service.getByIdSimple(id);
+        this.service.delete(reaction);
+
+        ResponseHttp<Object> res = new ResponseHttp<>(
+                null,
+                "Reaction deleted successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @Override
+    public ResponseEntity<?> update(
+            @IsId @PathVariable Long id,
+            @Valid @RequestBody UpdateReactionDTO dto,
+            HttpServletRequest request
+    ) {
+        ReactionModel reaction = this.service.getByIdSimple(id);
+        ReactionModel update = this.service.update(dto, reaction);
+
+        ResponseHttp<ReactionDTO> res = new ResponseHttp<>(
+                this.mapper.toDTO(update),
+                "Reaction updated successfully",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
 
 }

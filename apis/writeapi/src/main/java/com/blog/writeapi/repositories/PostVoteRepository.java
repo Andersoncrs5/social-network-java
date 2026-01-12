dc351befbb0d9ea2cceaf115064a3b99e56d07f3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-12 12:44:08 -0300

    Set up PostVoteRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostVoteRepository.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi.repositories;
+
+import com.blog.writeapi.models.PostModel;
+import com.blog.writeapi.models.PostVoteModel;
+import com.blog.writeapi.models.UserModel;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostVoteRepository extends JpaRepository<@NonNull PostVoteModel, @NonNull Long> {
+    Optional<PostVoteModel> findByUserAndPost(UserModel user, PostModel post);
+}

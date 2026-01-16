28676f436e6b1978efdfecd80c4274855abe5466
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-16 15:04:20 -0300

    New methods

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostReactionService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostReactionService.java
@@ -1,4 +1,16 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.models.PostModel;
+import com.blog.writeapi.models.PostReactionModel;
+import com.blog.writeapi.models.ReactionModel;
+import com.blog.writeapi.models.UserModel;
+
+import java.util.Optional;
+
 public interface IPostReactionService {
+    Optional<PostReactionModel> findByPostAndUser(PostModel post, UserModel user);
+    Boolean existsByPostAndUser(PostModel post, UserModel user);
+    void delete(PostReactionModel model);
+    PostReactionModel create(PostModel post, ReactionModel reaction, UserModel user);
+    PostReactionModel updateSimple(PostReactionModel model);
 }

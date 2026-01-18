80e445859106ef25ff998030fc102e3fb7700ac3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-18 15:38:28 -0300

    New method findByUserAndComment

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/CommentReactionRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/CommentReactionRepository.java
@@ -1,8 +1,13 @@
 package com.blog.writeapi.repositories;
 
+import com.blog.writeapi.models.CommentModel;
 import com.blog.writeapi.models.CommentReactionModel;
+import com.blog.writeapi.models.UserModel;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentReactionRepository extends JpaRepository<@NonNull CommentReactionModel, @NonNull Long> {
+    Optional<CommentReactionModel> findByUserAndComment(UserModel user, CommentModel comment);
 }

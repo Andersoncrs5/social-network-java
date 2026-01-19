b90801a51241797a0077ab37f1debe82c14e3a65
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-19 15:48:07 -0300

    Added table base  AttachmentBaseModel

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/bases/models/AttachmentBaseModel.java
@@ -0,0 +1,30 @@
+package com.blog.writeapi.utils.bases.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+public abstract class AttachmentBaseModel extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String storageKey;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    private String contentType;
+
+    private Long fileSize;
+
+}

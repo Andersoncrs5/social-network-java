fa84f50fba05b1c19239229ce1f5ab5618f9472e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-19 14:50:01 -0300

    Added validations

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/userProfile/UpdateUserProfileDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/userProfile/UpdateUserProfileDTO.java
@@ -1,12 +1,15 @@
 package com.blog.writeapi.dtos.userProfile;
 
-import com.blog.writeapi.dtos.user.UserDTO;
 import com.blog.writeapi.models.enums.profile.ProfileVisibilityEnum;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
 public record UpdateUserProfileDTO(
+        @Size(max = 800)
         String bio,
+
+        @Size(max = 500)
         String avatarUrl,
         Set<String> websiteUrls,
         ProfileVisibilityEnum visibility

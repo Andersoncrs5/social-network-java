4b41e318b06f278a3035cd81154dc6346794c73a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-25 20:23:54 -0300

    Removed import useless

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/bases/dto/BaseDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/bases/dto/BaseDTO.java
@@ -1,10 +1,7 @@
 package com.blog.writeapi.utils.bases.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import java.time.OffsetDateTime;
 

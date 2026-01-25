f016d9759ebdcb8a7568eeabe68635fd43b4ea3e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-25 20:24:16 -0300

    Added methods

--- apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostAttachmentService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/services/interfaces/IPostAttachmentService.java
@@ -1,4 +1,18 @@
 package com.blog.writeapi.services.interfaces;
 
+import com.blog.writeapi.dtos.postAttachment.CreatePostAttachmentDTO;
+import com.blog.writeapi.dtos.postAttachment.UpdatePostAttachmentDTO;
+import com.blog.writeapi.models.PostAttachmentModel;
+import com.blog.writeapi.models.PostModel;
+import com.blog.writeapi.models.UserModel;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import com.blog.writeapi.utils.annotations.valid.isModelInitialized.IsModelInitialized;
+
+import java.util.Optional;
+
 public interface IPostAttachmentService {
+    PostAttachmentModel getByIdSimple(@IsId Long id);
+    Boolean delete(@IsModelInitialized PostAttachmentModel model);
+    Optional<PostAttachmentModel> create(CreatePostAttachmentDTO dto, @IsModelInitialized UserModel user, @IsModelInitialized PostModel post);
+    PostAttachmentModel updateMetadata(Long id, UpdatePostAttachmentDTO dto);
 }

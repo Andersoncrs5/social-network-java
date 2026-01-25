d9b7b1ae8107bb701fd99b968472ead0a6ddb1a2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-25 20:50:29 -0300

    New columns isPublic and isVisible

--- apis/writeapi/src/main/java/com/blog/writeapi/dtos/postAttachment/UpdatePostAttachmentDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/dtos/postAttachment/UpdatePostAttachmentDTO.java
@@ -1,7 +1,8 @@
 package com.blog.writeapi.dtos.postAttachment;
 
 public record UpdatePostAttachmentDTO(
-        String fileName
+        String fileName,
+        Boolean isVisible,
+        Boolean isPublic
 ) {
-
 }

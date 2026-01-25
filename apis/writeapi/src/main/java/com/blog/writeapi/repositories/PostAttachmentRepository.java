8b243fcbf9042657f1730607fe186843630c1342
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-25 20:42:39 -0300

    New method findAllByPost

--- apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostAttachmentRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/repositories/PostAttachmentRepository.java
@@ -1,8 +1,12 @@
 package com.blog.writeapi.repositories;
 
 import com.blog.writeapi.models.PostAttachmentModel;
+import com.blog.writeapi.models.PostModel;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostAttachmentRepository extends JpaRepository<@NotNull PostAttachmentModel, @NotNull Long> {
+    List<PostAttachmentModel> findAllByPost(PostModel post);
 }

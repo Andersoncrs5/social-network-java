2f9e7bcbdf735cbc1e5a28e933c906df8973c4d4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-26 09:43:07 -0300

    Little improvements

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/PostAttachmentControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/PostAttachmentControllerDocs.java
@@ -1,4 +1,14 @@
 package com.blog.writeapi.controllers.docs;
 
+import com.blog.writeapi.dtos.postAttachment.CreatePostAttachmentDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface PostAttachmentControllerDocs {
+    ResponseEntity<?> create(
+            @Valid @RequestBody CreatePostAttachmentDTO dto,
+            HttpServletRequest request
+    );
 }

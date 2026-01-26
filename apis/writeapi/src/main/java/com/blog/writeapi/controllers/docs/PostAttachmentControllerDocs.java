70b4e43e631f938561eac72302a71dd6fc1766cd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-26 14:06:17 -0300

    new method delete

--- apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/PostAttachmentControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/controllers/docs/PostAttachmentControllerDocs.java
@@ -1,14 +1,30 @@
 package com.blog.writeapi.controllers.docs;
 
 import com.blog.writeapi.dtos.postAttachment.CreatePostAttachmentDTO;
+import com.blog.writeapi.utils.annotations.valid.global.isId.IsId;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface PostAttachmentControllerDocs {
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @CircuitBreaker(name = "tag-upload-file-cb")
     ResponseEntity<?> create(
             @Valid @RequestBody CreatePostAttachmentDTO dto,
+            HttpServletRequest request
+    );
+
+    @DeleteMapping("{id}")
+    @CircuitBreaker(name = "tagDeleteCB")
+    ResponseEntity<?> delete(
+            @PathVariable @IsId Long id,
             HttpServletRequest request
     );
 }

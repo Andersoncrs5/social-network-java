01ee7be4468faeff8728e108a3b19d208c322782
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-06 16:30:25 -0300

    Remake

--- init-s3.sh
+++ init-s3.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-awslocal s3api head-bucket --bucket attachments 2>/dev/null || awslocal s3 mb s3://attachments
-echo "Bucket criado com sucesso!"
+
+create_s3_bucket() {
+  local BUCKET_NAME=$1
+  local ENABLE_VERSIONING=$2
+
+  echo "Verificando bucket: $BUCKET_NAME"
+
+  # Verifica se o bucket já existe para evitar erro de 'BucketAlreadyOwnedByYou'
+  if ! awslocal s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+    echo "Criando bucket: $BUCKET_NAME..."
+    awslocal s3 mb "s3://$BUCKET_NAME"
+
+    if [ "$ENABLE_VERSIONING" = "true" ]; then
+      echo "Ativando versionamento para: $BUCKET_NAME"
+      awslocal s3api put-bucket-versioning --bucket "$BUCKET_NAME" --versioning-configuration Status=Enabled
+    fi
+  else
+    echo "Bucket $BUCKET_NAME já existe. Pulando criação."
+  fi
+}
+
+create_s3_bucket "attachments" "false"
+
+create_s3_bucket "archive-storage" "true"
+
+echo "S3 Initialized!"

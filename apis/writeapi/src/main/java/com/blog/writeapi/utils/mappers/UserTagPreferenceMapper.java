2d705f6c3cb32a9912618daac17e2bba76b6a427
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-14 22:04:30 -0300

    Set up UserTagPreferenceMapper

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/UserTagPreferenceMapper.java
@@ -0,0 +1,32 @@
+package com.blog.writeapi.utils.mappers;
+
+import com.blog.writeapi.dtos.userTagPreference.UserTagPreferenceDTO;
+import com.blog.writeapi.models.UserTagPreferenceModel;
+import com.blog.writeapi.utils.annotations.valid.isModelInitialized.IsModelInitialized;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                TagMapper.class, UserMapper.class
+        },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface UserTagPreferenceMapper {
+
+    UserTagPreferenceDTO toDTO(@IsModelInitialized UserTagPreferenceModel model);
+
+    default OffsetDateTime map(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.atOffset(ZoneOffset.UTC);
+    }
+
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : offsetDateTime.toLocalDateTime();
+    }
+
+}

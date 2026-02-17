ca3104e22a64495272862680fb6f32309f605d57
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-17 13:16:36 -0300

    New enum ModerationActionType

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/enums/report/ModerationActionType.java
@@ -0,0 +1,11 @@
+package com.blog.writeapi.utils.enums.report;
+
+public enum ModerationActionType {
+    NONE,
+    CONTENT_REMOVED,
+    CONTENT_HIDDEN,
+    USER_WARNED,
+    USER_SUSPENDED,
+    USER_BANNED,
+    SHADOW_BANNED
+}

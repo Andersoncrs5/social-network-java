aac959d96764d4ddbee95e90d4d8da69b32e02cf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-17 13:16:50 -0300

    New enum ReportReason

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/enums/report/ReportReason.java
@@ -0,0 +1,13 @@
+package com.blog.writeapi.utils.enums.report;
+
+public enum ReportReason {
+    SPAM,
+    HATE_SPEECH,
+    HARASSMENT,
+    VIOLENCE,
+    NUDITY_OR_SEXUAL_CONTENT,
+    MISINFORMATION,
+    INTELLECTUAL_PROPERTY,
+    SELF_HARM,
+    OTHER
+}

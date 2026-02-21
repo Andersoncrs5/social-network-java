7c597c4064bdb0be76c015cd23c0e5e26a3e04b8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-21 11:07:24 -0300

    New column bannerUrl and loginBlockAt

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/user/dtos/UserDTO.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/user/dtos/UserDTO.java
@@ -1,12 +1,15 @@
 package com.blog.writeapi.modules.user.dtos;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public record UserDTO(
         Long id,
         String name,
         String username,
         String email,
+        String bannerUrl,
+        OffsetDateTime loginBlockAt,
         LocalDateTime createdAt
 ) {
 }

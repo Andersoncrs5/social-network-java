bfcec459e9a51080bc79a608f96f78bce2f277b9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-21 11:10:28 -0300

    Fix anything

--- apis/writeapi/src/main/java/com/blog/writeapi/configs/startup/SuperAdmInitializr.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/startup/SuperAdmInitializr.java
@@ -1,5 +1,6 @@
 package com.blog.writeapi.configs.startup;
 
+import cn.hutool.core.lang.Snowflake;
 import com.blog.writeapi.modules.role.models.RoleModel;
 import com.blog.writeapi.modules.user.models.UserModel;
 import com.blog.writeapi.modules.user.repository.UserRepository;
@@ -9,61 +10,67 @@ import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Random;
+import java.util.Arrays;
 
 @Slf4j
 @Component
+@Order(2)
 @RequiredArgsConstructor
 public class SuperAdmInitializr implements CommandLineRunner {
 
     private final UserRepository repository;
-    private final RoleService roleRepository;
+    private final RoleService roleService;
     private final UserRoleService userRoleService;
     private final Argon2PasswordEncoder encoder;
+    private final Snowflake snowflake;
 
     @Override
     @Transactional
-    @Retry(name = "super-adm-retry", fallbackMethod = "fallbackCreateCategory")
+    @Retry(name = "super-adm-retry", fallbackMethod = "fallbackCreateSuperAdm")
     public void run(String... args) throws Exception {
         String email = "system.domain@gmail.com";
 
         UserModel superAdm = repository.findByEmailIgnoreCase(email)
                 .orElseGet(() -> {
+                    log.info("Creating initial System User...");
                     UserModel newUser = new UserModel().toBuilder()
+                            .id(snowflake.nextId())
                             .name("System")
                             .username("System")
                             .email(email)
                             .password(this.encoder.encode("0123456789"))
                             .build();
-                    newUser.setId(new Random().nextLong(1, Long.MAX_VALUE));
                     return repository.save(newUser);
                 });
 
-        RoleModel role = roleRepository.findByName("SUPER_ADM_ROLE")
-                .orElseThrow(() -> new RuntimeException("Role SUPER_ADM_ROLE not found yet."));
-        applyRole(superAdm, role);
+        RoleModel superRole = roleService.findByName("SUPER_ADM_ROLE")
+                .orElseThrow(() -> new RuntimeException("Role SUPER_ADM_ROLE not found."));
+
+        RoleModel admRole = roleService.findByName("ADM_ROLE")
+                .orElseThrow(() -> new RuntimeException("Role ADM_ROLE not found."));
+
+        applyRoles(superAdm, superRole, admRole);
     }
 
-    private void applyRole(UserModel user, RoleModel role) {
-        if (user == null || role == null) return;
+    private void applyRoles(UserModel user, RoleModel... roles) {
+        Arrays.stream(roles).forEach(role -> {
+            boolean hasRole = this.userRoleService.existsByUserAndRole(user, role);
 
-        boolean hasRole = this.userRoleService.existsByUserAndRole(user, role);
-
-        if (hasRole) {
-            log.debug("User '{}' already has the role '{}'.", user.getEmail(), role.getName());
-            return;
-        }
-
-        this.userRoleService.create(user, role);
-        log.debug("Role '{}' assigned to user '{}'.", role.getName(), user.getEmail());
+            if (!hasRole) {
+                this.userRoleService.create(user, role);
+                log.info("Role '{}' assigned to user '{}'.", role.getName(), user.getEmail());
+            } else {
+                log.debug("User '{}' already has the role '{}'.", user.getEmail(), role.getName());
+            }
+        });
     }
 
-    public void fallbackCreateCategory(String[] args, Exception e) {
-        log.error("All attempts to create a category have failed.: {}", e.getMessage());
+    public void fallbackCreateSuperAdm(String[] args, Exception e) {
+        log.error("Critical failure initializing Super Admin: {}", e.getMessage());
     }
-
 }

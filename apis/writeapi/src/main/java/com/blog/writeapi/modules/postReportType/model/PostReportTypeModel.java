1caa9fef117dd97c46d72a11c501cb46ec0d78e7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-01 21:17:26 -0300

    New table PostReportTypeModel

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postReportType/model/PostReportTypeModel.java
@@ -0,0 +1,44 @@
+package com.blog.writeapi.modules.postReportType.model;
+
+import com.blog.writeapi.modules.reportPost.model.PostReportModel;
+import com.blog.writeapi.utils.bases.models.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(
+        name = "post_reaction_types",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_post_reaction_types", columnNames = {"post_id", "type_id"})
+        }
+)
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class PostReportTypeModel extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "report_id",
+            nullable = false,
+            columnDefinition = "BIGINT UNSIGNED"
+    )
+    private PostReportModel report;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "type_id",
+            nullable = false,
+            columnDefinition = "BIGINT UNSIGNED"
+    )
+    private PostReportTypeModel type;
+
+}

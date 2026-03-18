74a231220e0b919a7d5d098134b38460b2e931bc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-18 09:26:55 -0300

    Set up PostReportTypeService

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postReportType/service/provider/PostReportTypeService.java
@@ -0,0 +1,78 @@
+package com.blog.writeapi.modules.postReportType.service.provider;
+
+import cn.hutool.core.lang.Snowflake;
+import com.blog.writeapi.modules.postReportType.model.PostReportTypeModel;
+import com.blog.writeapi.modules.postReportType.repository.PostReportTypeRepository;
+import com.blog.writeapi.modules.postReportType.service.interfaces.IPostReportTypeService;
+import com.blog.writeapi.modules.reportPost.model.PostReportModel;
+import com.blog.writeapi.modules.reportType.model.ReportTypeModel;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
+import com.blog.writeapi.utils.classes.ResultToggle;
+import com.blog.writeapi.utils.enums.global.ToggleEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostReportTypeService implements IPostReportTypeService {
+
+    private final PostReportTypeRepository repository;
+    private final Snowflake generator;
+
+    public Optional<PostReportTypeModel> getById(@IsId Long id) {
+        return this.repository.findById(id);
+    }
+
+    public void delete(@IsModelInitialized PostReportTypeModel type) {
+        this.repository.delete(type);
+    }
+
+    public PostReportTypeModel create(
+            @IsModelInitialized PostReportModel report,
+            @IsModelInitialized ReportTypeModel type
+    ) {
+        PostReportTypeModel model = new PostReportTypeModel().toBuilder()
+                .id(generator.nextId())
+                .report(report)
+                .type(type)
+                .build();
+
+        return this.repository.save(model);
+    }
+
+    public Boolean existsByReportAndType(
+            @IsModelInitialized PostReportModel report,
+            @IsModelInitialized ReportTypeModel type
+    ) {
+        return this.repository.existsByReportAndType(report, type);
+    }
+
+    public ResultToggle<PostReportTypeModel> toggle(
+            @IsModelInitialized PostReportModel report,
+            @IsModelInitialized ReportTypeModel type
+    ) {
+        Optional<PostReportTypeModel> exists = this.repository.findByReportAndType(report, type);
+
+        if (exists.isPresent()) {
+            this.repository.delete(exists.get());
+            return new ResultToggle<>(exists, ToggleEnum.REMOVED);
+        }
+
+        PostReportTypeModel model = PostReportTypeModel.builder()
+            .id(generator.nextId())
+            .type(type)
+            .report(report)
+            .build();
+
+        PostReportTypeModel saved = this.repository.save(model);
+
+        return new ResultToggle<>(
+            Optional.of(saved),
+            ToggleEnum.ADDED
+        );
+    }
+
+}

86b04f3d62202030fb0826a269c9ac347b33611d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-19 17:53:58 -0300

    Impled CommentReportMapper

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/mappers/CommentReportMapper.java
@@ -0,0 +1,57 @@
+package com.blog.writeapi.utils.mappers;
+
+import com.blog.writeapi.modules.commentReport.dto.CommentReportDTO;
+import com.blog.writeapi.modules.commentReport.dto.CreateCommentReportDTO;
+import com.blog.writeapi.modules.commentReport.dto.UpdateCommentReportDTO;
+import com.blog.writeapi.modules.commentReport.model.CommentReportModel;
+import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                CommentMapper.class,
+                UserMapper.class
+        },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface CommentReportMapper {
+
+    CommentReportModel toModel(CommentReportDTO dto);
+
+    CommentReportDTO toDTO(@IsModelInitialized CommentReportModel model);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "moderator", ignore = true)
+    @Mapping(target = "moderatedAt", ignore = true)
+    @Mapping(target = "moderatorNotes", ignore = true)
+    CommentReportModel toModel(CreateCommentReportDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "comment", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "commentAuthorId", ignore = true)
+    @Mapping(target = "commentContentSnapshot", ignore = true)
+    @Mapping(target = "aiToxicityScore", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void merge(UpdateCommentReportDTO dto, @MappingTarget @IsModelInitialized CommentReportModel model);
+
+    default OffsetDateTime map(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.atOffset(ZoneOffset.UTC);
+    }
+
+    default LocalDateTime map(OffsetDateTime offsetDateTime) {
+        return offsetDateTime == null ? null : offsetDateTime.toLocalDateTime();
+    }
+}

f3a70ea846019dc188e997e0375f1e953cc2039b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-19 17:53:02 -0300

    Improvement

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/exceptions/BusinessRuleException.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/exceptions/BusinessRuleException.java
@@ -1,7 +1,19 @@
 package com.blog.writeapi.utils.exceptions;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class BusinessRuleException extends RuntimeException {
+    private final HttpStatus status;
+
     public BusinessRuleException(String message) {
-        super(message, null, false, false);
+        super(message);
+        this.status = HttpStatus.BAD_REQUEST;
+    }
+
+    public BusinessRuleException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
     }
 }

b26177dfc9f738595dcb1496371d05bd2036687d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-20 14:42:24 -0300

    Set up IPostViewControllerDocs

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postView/controller/docs/IPostViewControllerDocs.java
@@ -0,0 +1,18 @@
+package com.blog.writeapi.modules.postView.controller.docs;
+
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public interface IPostViewControllerDocs {
+
+    @PostMapping("/{postId}")
+    @CircuitBreaker(name = "tagCreateCB")
+    ResponseEntity<?> create(
+            @IsId @PathVariable Long postId,
+            HttpServletRequest request
+    );
+}

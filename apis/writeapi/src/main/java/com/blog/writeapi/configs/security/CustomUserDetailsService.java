22417c9ae6d65b361898127546948a36b6ed97e4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-21 12:13:21 -0300

    Improvement in method loadUserByUsername

--- apis/writeapi/src/main/java/com/blog/writeapi/configs/security/CustomUserDetailsService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/security/CustomUserDetailsService.java
@@ -4,29 +4,56 @@ import com.blog.writeapi.modules.user.models.UserModel;
 import com.blog.writeapi.modules.userRole.models.UserRoleModel;
 import com.blog.writeapi.modules.user.repository.UserRepository;
 import com.blog.writeapi.modules.userRole.repository.UserRoleRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+    private static final String PREFIX = "auth:user:";
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        String key = PREFIX + email;
+
+        try {
+            Object object = redisTemplate.opsForValue().get(key);
+
+            if (object != null) {
+                String json = object.toString();
+                return objectMapper.readValue(json, UserPrincipal.class);
+            }
+        } catch (Exception e) {
+            log.warn("Error the to read cache of Redis to {}: {}", email, e.getMessage());
+        }
+
         UserModel user = userRepository.findByEmailIgnoreCase(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         List<UserRoleModel> roles = this.userRoleRepository.findAllByUser(user);
 
-        return new UserPrincipal(
+        if (user.getLoginBlockAt() != null && user.getLoginBlockAt().isAfter(OffsetDateTime.now())) {
+            throw new DisabledException("User account is temporarily blocked until " + user.getLoginBlockAt());
+        }
+
+        UserPrincipal principal = new UserPrincipal(
                 user.getId(),
                 user.getEmail(),
                 user.getPassword(),
@@ -34,5 +61,36 @@ public class CustomUserDetailsService implements UserDetailsService {
                         .map(role -> new SimpleGrantedAuthority(role.getRole().getName()))
                         .toList()
         );
+
+        try {
+            String jsonToCache = objectMapper.writeValueAsString(principal);
+            redisTemplate.opsForValue().set(key, jsonToCache, Duration.ofMinutes(10));
+        } catch (Exception e) {
+            log.error("Erro ao salvar no Redis: {}", e.getMessage());
+        }
+
+        return principal;
     }
+
+
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        UserModel user = userRepository.findByEmailIgnoreCase(email)
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//
+//        List<UserRoleModel> roles = this.userRoleRepository.findAllByUser(user);
+//
+//        if (user.getLoginBlockAt() != null && user.getLoginBlockAt().isAfter(OffsetDateTime.now())) {
+//            throw new DisabledException("User account is temporarily blocked until " + user.getLoginBlockAt());
+//        }
+//
+//        return new UserPrincipal(
+//                user.getId(),
+//                user.getEmail(),
+//                user.getPassword(),
+//                roles.stream()
+//                        .map(role -> new SimpleGrantedAuthority(role.getRole().getName()))
+//                        .toList()
+//        );
+//    }
 }

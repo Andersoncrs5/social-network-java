3c3506f28e3a855872db62a20d7300e15afdf6bb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-23 14:58:16 -0300

    Set up UpdateUserSettingsDTO

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userSettings/dto/UpdateUserSettingsDTO.java
@@ -0,0 +1,25 @@
+package com.blog.writeapi.modules.userSettings.dto;
+
+import com.blog.writeapi.modules.user.dtos.UserDTO;
+import com.blog.writeapi.modules.userSettings.model.enums.ContentFilterLevelEnum;
+import com.blog.writeapi.modules.userSettings.model.enums.FontSizeScaleEnum;
+import com.blog.writeapi.modules.userSettings.model.enums.LanguageEnum;
+import com.blog.writeapi.modules.userSettings.model.enums.ThemeEnum;
+
+public record UpdateUserSettingsDTO(
+        LanguageEnum language,
+        ThemeEnum theme,
+        boolean showOnlineStatus,
+        boolean notifyNewFollower,
+        boolean notifyLikes,
+        boolean notifyComments,
+        boolean notifyMentions,
+        boolean twoFactorEnabled,
+        boolean autoplayVideos,
+        boolean marketingEmailsAllowed,
+        int itemsPerPage,
+        ContentFilterLevelEnum contentFilterLevel,
+        FontSizeScaleEnum fontSizeScale,
+        String timezone
+) {
+}

4eb4b7e60cb54be9c849283be32498759b2251c2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-23 14:55:28 -0300

    New params user, settingsDTO and roles

--- apis/writeapi/src/main/java/com/blog/writeapi/utils/res/ResponseTokens.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/res/ResponseTokens.java
@@ -1,7 +1,15 @@
 package com.blog.writeapi.utils.res;
 
+import com.blog.writeapi.modules.user.dtos.UserDTO;
+import com.blog.writeapi.modules.userSettings.dto.UserSettingsDTO;
+
+import java.util.List;
+
 public record ResponseTokens(
         String token,
-        String refreshToken
+        String refreshToken,
+        UserDTO user,
+        UserSettingsDTO settingsDTO,
+        List<String> roles
 ) {
 }

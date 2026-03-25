301e12f67a701b042769ed9c186d6e74a260ee3d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-25 09:24:46 -0300

    Set up UserBlockRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userBlock/repository/UserBlockRepository.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.modules.userBlock.repository;
+
+import com.blog.writeapi.modules.user.models.UserModel;
+import com.blog.writeapi.modules.userBlock.model.UserBlockModel;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserBlockRepository extends JpaRepository<UserBlockModel, Long> {
+    Optional<UserBlockModel> findByBlockerAndBlocked(
+            @IsModelInitialized UserModel blocker,
+            @IsModelInitialized UserModel blocked
+    );
+    Optional<UserBlockModel> findByBlockerIdAndBlockedId(
+            @IsId Long blockerId,
+            @IsId Long blockedId
+    );
+}

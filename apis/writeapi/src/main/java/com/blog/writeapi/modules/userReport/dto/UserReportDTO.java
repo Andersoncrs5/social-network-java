f3ee011f56a9c39b1bab3f543b474199bed7c4d5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-28 10:24:20 -0300

    Set up UserReportDTO

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userReport/dto/UserReportDTO.java
@@ -0,0 +1,28 @@
+package com.blog.writeapi.modules.userReport.dto;
+
+import com.blog.writeapi.modules.user.dtos.UserDTO;
+import com.blog.writeapi.utils.enums.report.ModerationActionType;
+import com.blog.writeapi.utils.enums.report.ReportReason;
+import com.blog.writeapi.utils.enums.report.ReportStatus;
+
+import java.time.OffsetDateTime;
+
+public record UserReportDTO(
+        Long id,
+        String description,
+        ReportStatus status,
+        ModerationActionType moderationActionType,
+        String moderatorNotes,
+        ReportReason reason,
+        OffsetDateTime resolvedAt,
+        UserDTO reportedUser,
+        UserDTO reporter,
+        UserDTO moderator,
+        Long postAuthorId,
+        String userProfileSnapshot,
+        Double aiTrustScoreAtReport,
+        Long version,
+        OffsetDateTime createdAt,
+        OffsetDateTime updatedAt
+) {
+}

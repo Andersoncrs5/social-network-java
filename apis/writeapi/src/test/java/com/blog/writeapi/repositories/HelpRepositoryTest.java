1c6d47334ab3afc01ba45ef8ac784852dfdf9198
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 09:01:21 -0300

    Setup HelpRepositoryTest

---
+++ apis/writeapi/src/test/java/com/blog/writeapi/repositories/HelpRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.blog.writeapi.repositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HelpRepositoryTest {
+}

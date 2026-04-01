a1b3c36f12df248da76c1ce241178e383e776e0c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 08:53:32 -0300

    Added RedisContainer

--- apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
@@ -1,9 +1,12 @@
 package com.blog.writeapi.configs;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfig {
@@ -17,4 +20,13 @@ public class TestContainerConfig {
                 .withPassword("test")
                 .withReuse(true);
     }
+
+    @Bean
+    @ServiceConnection
+    public RedisContainer redisContainer() {
+        return new RedisContainer(
+                DockerImageName.parse("redis:7.2")
+        ).withReuse(true);
+    }
+
 }

7a05686fe5a28dca27211748a8052276f33d3b86
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-09 20:10:28 -0300

    Set up PostReadingListModuleGateway

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postReadingList/gateway/PostReadingListModuleGateway.java
@@ -0,0 +1,26 @@
+package com.blog.writeapi.modules.postReadingList.gateway;
+
+import com.blog.writeapi.modules.post.models.PostModel;
+import com.blog.writeapi.modules.post.services.interfaces.IPostService;
+import com.blog.writeapi.modules.user.models.UserModel;
+import com.blog.writeapi.modules.user.service.docs.IUserService;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostReadingListModuleGateway {
+
+    private final IUserService userService;
+    private final IPostService postService;
+
+    public UserModel findUserById(@IsId Long id) {
+        return this.userService.GetByIdSimple(id);
+    }
+
+    public PostModel findPostById(@IsId Long id) {
+        return this.postService.getByIdSimple(id);
+    }
+
+}

3a9bbe7fd8159dd2524c6244db0f7b71bd110375
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-12 19:54:31 -0300

    Set up WebConfig

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/configs/security/WebConfig.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.configs.security;
+
+import com.blog.writeapi.modules.apiKeys.interceptors.ApiKeyAuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiKeyAuthInterceptor apiKeyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiKeyInterceptor)
+                .addPathPatterns("/v1/admin/api-keys/internal/**");
+    }
+}

4c7081bbd2e634a7465c3db860275c35893e20fc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-14 19:58:56 -0300

    New endpoint delete

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/stories/controller/docs/IStoryControllerDocs.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/stories/controller/docs/IStoryControllerDocs.java
@@ -3,19 +3,27 @@ package com.blog.writeapi.modules.stories.controller.docs;
 import com.blog.writeapi.configs.security.UserPrincipal;
 import com.blog.writeapi.modules.stories.dto.CreateStoryDTO;
 import com.blog.writeapi.modules.stories.dto.StoryDTO;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 public interface IStoryControllerDocs {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @CircuitBreaker(name = "tag-upload-file-cb")
     ResponseHttp<StoryDTO> create(
+            @RequestHeader("X-Idempotency-Key") String idempotencyKey,
             @Valid @ModelAttribute CreateStoryDTO dto,
             @AuthenticationPrincipal UserPrincipal principal
+    );
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> delete(
+            @PathVariable @IsId Long id,
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestHeader("X-Idempotency-Key") String idempotencyKey
     );
 }

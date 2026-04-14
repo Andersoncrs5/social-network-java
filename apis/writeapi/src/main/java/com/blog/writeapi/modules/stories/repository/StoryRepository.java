b6b92eb9f934672cc2198cd667fcca331e43e156
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-14 18:14:58 -0300

    Added StoryRepository

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/stories/repository/StoryRepository.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.modules.stories.repository;
+
+import com.blog.writeapi.modules.stories.model.StoryModel;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.OffsetDateTime;
+
+public interface StoryRepository extends JpaRepository<@NonNull StoryModel, @NonNull Long> {
+    @Modifying
+    @Query("UPDATE StoryModel s SET s.isArchived = true WHERE s.expiresAt < :now AND s.isArchived = false")
+    int archiveAllExpired(@Param("now") OffsetDateTime now);
+
+    Page<StoryModel> findAllByExpiresAtBeforeAndIsArchivedFalse(OffsetDateTime now, Pageable pageable);
+}

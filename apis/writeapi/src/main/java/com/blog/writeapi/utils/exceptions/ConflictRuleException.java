34446a3171c4226ffea6d7a0d195a87052943804
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-14 18:13:19 -0300

    New exception ConflictRuleException

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/exceptions/ConflictRuleException.java
@@ -0,0 +1,7 @@
+package com.blog.writeapi.utils.exceptions;
+
+public class ConflictRuleException extends RuntimeException {
+    public ConflictRuleException(String message) {
+        super(message);
+    }
+}

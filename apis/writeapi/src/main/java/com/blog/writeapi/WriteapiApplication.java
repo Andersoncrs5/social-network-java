c669b41843938ddb0d15a185d8743a354afeb6b2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-14 14:32:07 -0300

    Added EnableScheduling in WriteapiApplication

--- apis/writeapi/src/main/java/com/blog/writeapi/WriteapiApplication.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/WriteapiApplication.java
@@ -2,9 +2,10 @@ package com.blog.writeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WriteapiApplication {
 
 	public static void main(String[] args) {

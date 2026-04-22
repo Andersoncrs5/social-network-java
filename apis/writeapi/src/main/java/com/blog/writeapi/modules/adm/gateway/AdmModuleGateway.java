5ec652ac7f1c7839082e22ac196565090cbbdc7f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-22 10:54:01 -0300

    Set up AdmModuleGateway

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/adm/gateway/AdmModuleGateway.java
@@ -0,0 +1,9 @@
+package com.blog.writeapi.modules.adm.gateway;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdmModuleGateway {
+}

72af8920f66fdc05c4b1a151e36e818d3b6d3cd4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-22 10:53:28 -0300

    New method toggle

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/postVote/service/docs/IPostVoteService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postVote/service/docs/IPostVoteService.java
@@ -5,6 +5,8 @@ import com.blog.writeapi.modules.post.models.PostModel;
 import com.blog.writeapi.modules.postVote.models.PostVoteModel;
 import com.blog.writeapi.modules.user.models.UserModel;
 import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
+import com.blog.writeapi.utils.classes.ResultToggle;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Optional;
 
@@ -13,4 +15,9 @@ public interface IPostVoteService {
     void delete(@IsModelInitialized PostVoteModel vote);
     PostVoteModel create(TogglePostVoteDTO dto, @IsModelInitialized UserModel user, @IsModelInitialized PostModel post);
     PostVoteModel updateSimple(@IsModelInitialized PostVoteModel vote);
+    ResultToggle<PostVoteModel> toggle(
+            @IsModelInitialized UserModel user,
+            @IsModelInitialized PostModel post,
+            @NotNull TogglePostVoteDTO dto
+    );
 }

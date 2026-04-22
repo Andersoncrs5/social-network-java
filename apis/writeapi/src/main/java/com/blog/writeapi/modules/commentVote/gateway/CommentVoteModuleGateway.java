28a6249686b6bb4fef654c680888ef5b45922ac0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-22 10:53:43 -0300

    Set up CommentVoteModuleGateway

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/commentVote/gateway/CommentVoteModuleGateway.java
@@ -0,0 +1,20 @@
+package com.blog.writeapi.modules.commentVote.gateway;
+
+import com.blog.writeapi.modules.userBlock.service.docs.IUserBlockService;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentVoteModuleGateway {
+
+    private final IUserBlockService userBlockService;
+
+    public boolean isBlocked(
+            @IsId Long blockerId,
+            @IsId Long blockedId
+    ) {
+        return this.userBlockService.isBlocked(blockerId, blockedId);
+    }
+}

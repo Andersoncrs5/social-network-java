675df787a01266e9af6817d054d966d40d6dbddb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-22 10:53:49 -0300

    Set up CommentReactionModuleGateway

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/commentReaction/gateway/CommentReactionModuleGateway.java
@@ -0,0 +1,21 @@
+package com.blog.writeapi.modules.commentReaction.gateway;
+
+import com.blog.writeapi.modules.userBlock.service.docs.IUserBlockService;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentReactionModuleGateway {
+
+    private final IUserBlockService userBlockService;
+
+    public boolean isBlocked(
+            @IsId Long blockerId,
+            @IsId Long blockedId
+    ) {
+        return this.userBlockService.isBlocked(blockerId, blockedId);
+    }
+
+}

8189b0136fbdec5e6319e4f672a6afac373bbc2d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-22 10:43:02 -0300

    Rewrite method create

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/userCategoryPreference/service/providers/UserCategoryPreferenceService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userCategoryPreference/service/providers/UserCategoryPreferenceService.java
@@ -7,7 +7,11 @@ import com.blog.writeapi.modules.user.models.UserModel;
 import com.blog.writeapi.modules.userCategoryPreference.repository.UserCategoryPreferenceRepository;
 import com.blog.writeapi.modules.userCategoryPreference.service.docs.IUserCategoryPreferenceService;
 import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
+import com.blog.writeapi.utils.exceptions.BusinessRuleException;
+import com.blog.writeapi.utils.exceptions.InternalServerErrorException;
+import com.blog.writeapi.utils.exceptions.UniqueConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -37,13 +41,29 @@ public class UserCategoryPreferenceService implements IUserCategoryPreferenceSer
             @IsModelInitialized UserModel user,
             @IsModelInitialized CategoryModel category
     ) {
+        if (!category.getIsActive()) throw new BusinessRuleException("Category is unactive");
+
         UserCategoryPreferenceModel model = new UserCategoryPreferenceModel().toBuilder()
                 .id(this.snowflake.nextId())
                 .category(category)
                 .user(user)
                 .build();
 
-        return repository.save(model);
+        try {
+            return repository.save(model);
+        } catch (DataIntegrityViolationException e) {
+            String message = Optional.of(e.getMostSpecificCause())
+                    .map(Throwable::getMessage)
+                    .orElse("");
+
+            if (message.contains("uk_user_category_preferences")) {
+                throw new UniqueConstraintViolationException("This category has already been added .");
+            }
+
+            throw new BusinessRuleException("Database integrity error: " + message);
+        } catch (Exception e) {
+            throw new InternalServerErrorException("Error creating report association.");
+        }
     }
 
 

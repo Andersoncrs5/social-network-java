4ca4d34c806ba2a7ae9b6c9205fdd4b3d996142f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 14:06:47 -0300

    new methods

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/userRole/repository/UserRoleRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userRole/repository/UserRoleRepository.java
@@ -3,9 +3,12 @@ package com.blog.writeapi.modules.userRole.repository;
 import com.blog.writeapi.modules.role.models.RoleModel;
 import com.blog.writeapi.modules.user.models.UserModel;
 import com.blog.writeapi.modules.userRole.models.UserRoleModel;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
 import com.blog.writeapi.utils.annotations.validations.isModelInitialized.IsModelInitialized;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +19,12 @@ public interface UserRoleRepository extends JpaRepository<@NonNull UserRoleModel
     Boolean existsByUserAndRole(@IsModelInitialized UserModel user, @IsModelInitialized RoleModel role);
     Optional<UserRoleModel> findByUserAndRole(@IsModelInitialized UserModel user, @IsModelInitialized RoleModel role);
     List<UserRoleModel> findAllByUser(@IsModelInitialized UserModel user);
+
+    boolean existsByUserIdAndRoleId(Long userId, Long roleId);
+
+    Optional<UserRoleModel> findByUserIdAndRoleId(Long userId, Long roleId);
+
+    @Modifying
+    @Query("DELETE FROM UserRoleModel e WHERE e.id = :id")
+    int deleteByID(@IsId Long id);
 }

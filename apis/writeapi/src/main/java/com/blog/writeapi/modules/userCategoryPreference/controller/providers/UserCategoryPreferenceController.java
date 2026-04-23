c22b38f1a83c8a04130fbcbc0b67687fe08c1c39
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 20:02:04 -0300

    Improvement toggle

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/userCategoryPreference/controller/providers/UserCategoryPreferenceController.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userCategoryPreference/controller/providers/UserCategoryPreferenceController.java
@@ -1,31 +1,26 @@
 package com.blog.writeapi.modules.userCategoryPreference.controller.providers;
 
+import com.blog.writeapi.configs.api.idempotent.Idempotent;
 import com.blog.writeapi.configs.security.UserPrincipal;
 import com.blog.writeapi.modules.userCategoryPreference.controller.docs.UserCategoryPreferenceControllerDocs;
 import com.blog.writeapi.modules.userCategoryPreference.dtos.UserCategoryPreferenceDTO;
-import com.blog.writeapi.modules.category.models.CategoryModel;
 import com.blog.writeapi.modules.userCategoryPreference.models.UserCategoryPreferenceModel;
-import com.blog.writeapi.modules.user.models.UserModel;
-import com.blog.writeapi.modules.category.service.docs.ICategoryService;
-import com.blog.writeapi.utils.services.interfaces.ITokenService;
 import com.blog.writeapi.modules.userCategoryPreference.service.docs.IUserCategoryPreferenceService;
-import com.blog.writeapi.modules.user.service.docs.IUserService;
 import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import com.blog.writeapi.utils.classes.ResultToggle;
+import com.blog.writeapi.utils.enums.global.ToggleEnum;
 import com.blog.writeapi.utils.mappers.UserCategoryPreferenceMapper;
 import com.blog.writeapi.utils.res.ResponseHttp;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.http.HttpStatusCode;
-
-import java.time.OffsetDateTime;
-import java.util.Optional;
-import java.util.UUID;
 
 @Validated
 @RestController
@@ -34,46 +29,30 @@ import java.util.UUID;
 public class UserCategoryPreferenceController implements UserCategoryPreferenceControllerDocs {
 
     private final IUserCategoryPreferenceService service;
-    private final ICategoryService categoryService;
-    private final IUserService userService;
-    private final ITokenService tokenService;
     private final UserCategoryPreferenceMapper mapper;
 
     @Override
+    @Idempotent
     public ResponseEntity<?> toggle(
             @PathVariable @IsId Long categoryId,
             HttpServletRequest request,
-            @AuthenticationPrincipal UserPrincipal principal
+            @AuthenticationPrincipal UserPrincipal principal,
+            @RequestHeader("X-Idempotency-Key") String idempotencyKey
     ) {
-        UserModel user = principal.getUser();
-        CategoryModel category = this.categoryService.getByIdSimple(categoryId);
+        ResultToggle<UserCategoryPreferenceModel> toggle = this.service.toggle(categoryId, principal.getId());
 
-        Optional<UserCategoryPreferenceModel> optional = this.service.getByUserAndCategory(user, category);
+        String message = toggle.result() == ToggleEnum.ADDED
+                ? "Category added" : "Category removed";
 
-        if (optional.isPresent()) {
-            this.service.delete(optional.get());
+        HttpStatus status = toggle.result() == ToggleEnum.ADDED
+                ? HttpStatus.CREATED : HttpStatus.OK;
 
-            return ResponseEntity.status(HttpStatusCode.OK).body(new ResponseHttp<>(
-                    null,
-                    "Category removed with successfully",
-                    UUID.randomUUID().toString(),
-                    1,
-                    true,
-                    OffsetDateTime.now()
-            ));
-        }
+        UserCategoryPreferenceDTO dto = this.mapper.toDTO(toggle.body().orElse(null));
 
-        UserCategoryPreferenceModel model = this.service.create(user, category);
-
-        UserCategoryPreferenceDTO dto = this.mapper.toDTO(model);
-
-        return ResponseEntity.status(HttpStatusCode.CREATED).body(new ResponseHttp<>(
-                dto,
-            "Category added with successfully",
-            UUID.randomUUID().toString(),
-            1,
-            true,
-            OffsetDateTime.now()
+        return ResponseEntity.status(status).body(ResponseHttp.success(
+            dto,
+            message,
+            idempotencyKey
         ));
     }
 

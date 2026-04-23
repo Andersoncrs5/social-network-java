3ca10c608eb5b13fff0f331bff282b351ddc0cbd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 14:05:37 -0300

    New method addRoleToUser, removeRoleToUser and toggleRoleAdmInUser

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/adm/service/docs/IAdmService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/adm/service/docs/IAdmService.java
@@ -1,4 +1,16 @@
 package com.blog.writeapi.modules.adm.service.docs;
 
+import com.blog.writeapi.modules.adm.dto.ToggleRoleDTO;
+import com.blog.writeapi.modules.userRole.models.UserRoleModel;
+import com.blog.writeapi.utils.classes.ResultToggle;
+import com.blog.writeapi.utils.result.Result;
+
 public interface IAdmService {
+    Result<UserRoleModel> addRoleToUser(
+            ToggleRoleDTO dto
+    );
+    Result<Void> removeRoleToUser(
+            ToggleRoleDTO dto
+    );
+    ResultToggle<UserRoleModel> toggleRoleAdmInUser(ToggleRoleDTO dto, Long currentUserId);
 }

6bef02a775789285f5c7f8377fbe6be8e70c4f25
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 20:00:17 -0300

    new method deleteAndCount

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/apiKeys/repository/ApiKeyRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/apiKeys/repository/ApiKeyRepository.java
@@ -3,9 +3,17 @@ package com.blog.writeapi.modules.apiKeys.repository;
 import com.blog.writeapi.modules.apiKeys.model.ApiKeyModel;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ApiKeyRepository extends JpaRepository<ApiKeyModel, Long> {
     Optional<ApiKeyModel> findByKeyHashAndActiveTrue(@NotBlank String keyHash);
+
+    @Modifying
+    @Query("DELETE FROM ApiKeyModel s WHERE s.id = :id")
+    int deleteAndCount(@Param("id") Long id);
+
 }

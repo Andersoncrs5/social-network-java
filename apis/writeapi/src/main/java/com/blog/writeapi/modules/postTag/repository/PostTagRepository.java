f65d185ebec0d0e50c6654be6e2f1fc533e0fc05
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 18:33:56 -0300

    Added method deleteAndCount

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/postTag/repository/PostTagRepository.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/postTag/repository/PostTagRepository.java
@@ -4,7 +4,15 @@ import com.blog.writeapi.modules.postTag.models.PostTagModel;
 import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostTagRepository extends JpaRepository<@NonNull PostTagModel, @NonNull Long> {
     Boolean existsByPostIdAndTagId(@IsId Long postId, @IsId Long tagId);
+
+    @Modifying
+    @Query("DELETE FROM PostTagModel e WHERE e.id = :id")
+    int deleteAndCount(@Param("id") @IsId Long id);
+
 }

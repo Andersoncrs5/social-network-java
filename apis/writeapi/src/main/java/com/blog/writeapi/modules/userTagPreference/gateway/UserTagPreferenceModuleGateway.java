dc4dc20862eef65ecaf8e8d2d53a051607c65c37
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-23 20:50:57 -0300

    New method findUserById and findTagById

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/userTagPreference/gateway/UserTagPreferenceModuleGateway.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/userTagPreference/gateway/UserTagPreferenceModuleGateway.java
@@ -1,4 +1,26 @@
 package com.blog.writeapi.modules.userTagPreference.gateway;
 
+import com.blog.writeapi.modules.tag.models.TagModel;
+import com.blog.writeapi.modules.tag.service.docs.ITagService;
+import com.blog.writeapi.modules.user.models.UserModel;
+import com.blog.writeapi.modules.user.service.docs.IUserService;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserTagPreferenceModuleGateway {
+
+    private final IUserService userService;
+    private final ITagService tagService;
+
+    public UserModel findUserById(@IsId Long id) {
+        return userService.GetByIdSimple(id);
+    }
+
+    public TagModel findTagById(@IsId Long id) {
+        return tagService.getByIdSimple(id);
+    }
+
 }

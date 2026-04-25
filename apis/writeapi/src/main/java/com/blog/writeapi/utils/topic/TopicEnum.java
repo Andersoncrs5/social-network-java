6498b27fb045b5ebce7396e60f7ed21c4ea14f0d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-25 11:31:22 -0300

    Set up TopicEnum

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/utils/topic/TopicEnum.java
@@ -0,0 +1,16 @@
+package com.blog.writeapi.utils.topic;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TopicEnum {
+    POST_CREATED("posts.created"),
+    POST_METRIC("posts.metric"),
+    COMMENT_ADDED("comments.added"),
+    COMMENT_METRIC("comments.metric"),
+    USER_REGISTERED("users.registered");
+
+    private final String topicName;
+}

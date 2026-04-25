247ff2049b6be9c6b3b899c845b5f9415941d370
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-25 11:32:36 -0300

    Set up OutboxModuleGateway

---
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/outbox/gateway/OutboxModuleGateway.java
@@ -0,0 +1,9 @@
+package com.blog.writeapi.modules.outbox.gateway;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxModuleGateway {
+}

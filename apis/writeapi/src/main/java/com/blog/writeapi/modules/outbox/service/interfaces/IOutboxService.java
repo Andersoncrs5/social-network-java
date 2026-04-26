4d104454844ac928571ad19d51ba4b8a367d9bb0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-26 10:37:54 -0300

    Added methods

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/outbox/service/interfaces/IOutboxService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/outbox/service/interfaces/IOutboxService.java
@@ -1,4 +1,12 @@
 package com.blog.writeapi.modules.outbox.service.interfaces;
 
+import com.blog.writeapi.modules.outbox.dto.CreateOutboxDTO;
+import com.blog.writeapi.modules.outbox.dto.UpdateOutboxDTO;
+import com.blog.writeapi.modules.outbox.model.OutboxModel;
+import com.blog.writeapi.utils.annotations.validations.global.isId.IsId;
+
 public interface IOutboxService {
+    OutboxModel create(CreateOutboxDTO dto);
+    void deleteById(@IsId Long id);
+    OutboxModel update(OutboxModel outbox, UpdateOutboxDTO dto);
 }

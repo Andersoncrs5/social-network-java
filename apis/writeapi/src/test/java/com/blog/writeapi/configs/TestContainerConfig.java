00ac55e8f48907e5543c4b4c6a1f97238a43edc4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-26 10:35:29 -0300

    commented kafka container

--- apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
@@ -4,10 +4,11 @@ import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
-
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfig {
 
@@ -24,16 +25,27 @@ public class TestContainerConfig {
     @Bean
     @ServiceConnection
     public RedisContainer redisContainer() {
-        return new RedisContainer(
-                DockerImageName.parse("redis:7.2")
-        ).withReuse(true);
+        return new RedisContainer(DockerImageName.parse("redis:7.2")).withReuse(true);
     }
 
-    @Bean
-    @ServiceConnection
-    public KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"))
-                .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true");
-    }
-
+//    // 1. Mudamos para estático para garantir que inicie antes do contexto
+//    private static final KafkaContainer kafkaContainer = new KafkaContainer(
+//            DockerImageName.parse("confluentinc/cp-kafka:7.6.1")
+//                    .asCompatibleSubstituteFor("apache/kafka"))
+//            .withEnv("KAFKA_NODE_ID", "1")
+//            .withEnv("KAFKA_PROCESS_ROLES", "broker,controller")
+//            .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093")
+//            .withEnv("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://0.0.0.0:9092")
+//            .withEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@localhost:9093")
+//            .withEnv("KAFKA_CONTROLLER_LISTENER_NAMES", "CONTROLLER");
+//
+//    static {
+//        kafkaContainer.start();
+//    }
+//
+//    // 2. Usamos o DynamicPropertyRegistry para injetar manualmente
+//    @DynamicPropertySource
+//    static void kafkaProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+//    }
 }

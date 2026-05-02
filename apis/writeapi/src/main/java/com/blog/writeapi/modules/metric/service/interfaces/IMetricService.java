39eb635400425f119b77ab49c59a165413f962ff
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-02 13:11:22 -0300

    New method handleEventUser

--- apis/writeapi/src/main/java/com/blog/writeapi/modules/metric/service/interfaces/IMetricService.java
+++ apis/writeapi/src/main/java/com/blog/writeapi/modules/metric/service/interfaces/IMetricService.java
@@ -2,8 +2,10 @@ package com.blog.writeapi.modules.metric.service.interfaces;
 
 import com.blog.writeapi.modules.metric.dto.CommentMetricEventDTO;
 import com.blog.writeapi.modules.metric.dto.PostMetricEventDTO;
+import com.blog.writeapi.modules.metric.dto.UserMetricEventDTO;
 
 public interface IMetricService {
     void handleEventPost(PostMetricEventDTO dto);
     void handleEventComment(CommentMetricEventDTO dto);
+    void handleEventUser(UserMetricEventDTO dto);
 }

435dae8890bdad25629c998258c49e25f04c1e83
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-03 11:31:05 -0300

    Removed imports useless

--- apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
+++ apis/writeapi/src/test/java/com/blog/writeapi/configs/TestContainerConfig.java
@@ -4,11 +4,9 @@ import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfig {
 
